ea2bd5696ad3dee7976591103360940ee07007d9
Dominic Santonil <dsantonil@ufl.edu>
2018-04-13 14:52:24 -0400

    Created event,h
    hope i did this header right, lmao

---
+++ event.h
@@ -0,0 +1,47 @@
+#ifndef EVENTS_H_
+#define EVENTS_H_
+
+#include <string>
+
+using namespace std;
+
+class event
+{
+      private:
+            string name;
+            string day;
+            int time;
+            int rest;
+            int length;
+            bool repeat;
+       
+       public:
+            event(string n, string d, int t, int r, int l, bool r,);
+            void setName(string n);
+            void setDay(string n);
+            void setTime(int n);
+            void setRest(int n);
+            void setLength(int n);
+            void setRepeat(int n);
+            string getName();
+            string getDay();
+            int getTime();
+            int getLength();
+            int getRest();
+            bool getRepeat();
+            
+}
+
+class assignment: public event
+{
+      private;
+            int modularity;
+            
+      public:
+            assignment (string n, string d, int t, int r, int l, bool r, int m);
+            void setModularity(int m);
+            int getModularity();
+            
+}
+
+#endif
