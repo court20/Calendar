591d33aab7dc7c97162352dd2194ef41d0bac6a5
Jonathan Huang <32203695+JonathanY-Huang@users.noreply.github.com>
2018-04-18 11:37:12 -0400

    Created foundation for the sorting algorithm

---
+++ sortAlg.cpp
@@ -0,0 +1,18 @@
+#include "day.h"
+#include "event.h"
+#include <vector>
+#include <string>
+using namespace std
+
+class sortAlg{
+	private:
+		day[] week;
+		vector<event> events;
+
+	public:
+		sortAlg(day[] w,vector<event> e){
+			week=w;
+			events=e;
+		}
+
+}
