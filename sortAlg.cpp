e7830cc4f93bc5f3c1ae89ec1b92318baf5977d4
salaar7 <32203612+salaar7@users.noreply.github.com>
2018-04-21 17:16:15 -0400

    fixed this class boys, esketit

--- sortAlg.cpp
+++ sortAlg.cpp
@@ -6,87 +6,79 @@
 
 using namespace std;
 
-class sortAlg{
-	private:
-	
-	/*
-	*data that we don't want to be changed
-	*we need this data to create the accruate schedule
-	*explicitely used
-	*/
-		user currentUser;
-		day[] week;
-		vector<event> events;
-		vector<event> chunks;
+class sortAlg {
 
-	public:
-		sortAlg(day[] w,vector<event> e,user u ){
-			week=w;
-			currentUser=u;
-			events=e;
-			chunks=chunk();
-			
-			for(int x=0; x<chunks.size();x++)
-			{
-				event largest= findLargest();
-				bool hasSpace= false;
+private:
+    user currentUser;
+    day* week;
+    vector<event> events;
+    vector<event> chunks;
 
-				for(int y=0; y<week.size();y++)
-				{
-					vector<event> temp=week[x].getEvents;
-					if(temp.size()==0){
-						if(abs(u.getWake()-u.getSleep()>largest.getLength())
-						   {
-							largest.setTime(u.getWake());
-							week[y].addEvent(largest);
-						}
-					}
-					else{
-						for(int z=0; z<temp.size(); z++)
-						{
-							largest.setTime(temp[z].getTime-temp[z].getLength());
-							if(week[y].addEvent(largest))
-							{
-								break;
-							}
-							largest.setTime(temp[z].getTime+temp[z].getLength());						
-							if(week[y].addEvent(largest))
-							{
-								break;
-							}						
-						}
-					}
-				}
-			}
-						
-		}
-		
-		void insertEvent(event e);
+public:
+    sortAlg(day w[],vector<event> e,user u ){
+        week=w;
+        currentUser=u;
+        events=e;
+        chunks=chunk();
 
-}
+        for(int x=0; x<chunks.size();x++)
+        {
+            event largest= findLargest();
+            bool hasSpace= false;
+
+            for(int y=0; y<week.size();y++)
+            {
+                vector<event> temp=week[x].getEvents;
+                if(temp.size()==0){
+                    if(abs(u.getWake()-u.getSleep()>largest.getLength()))
+                    {
+                        largest.setTime(u.getWake());
+                        week[y].addEvent(largest);
+                    }
+                }
+                else{
+                    for(int z=0; z<temp.size(); z++)
+                    {
+                        largest.setTime(temp[z].getTime-temp[z].getLength());
+                        if(week[y].addEvent(largest))
+                        {
+                            break;
+                        }
+                        largest.setTime(temp[z].getTime+temp[z].getLength());
+                        if(week[y].addEvent(largest))
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
 
 void insertEvent(event e)
 {
-	/*
+
+       /*
 	*use the user info on what day during the week the event is on
 	*if the day to compare lines up with the particular day in the week
 	*the event is then inserted into that day
 	*the time and information such as that it gets inserted into the correct place during the day
 	*the object itself holds its charastceristics
 	*/
-	
-	
-	string dayToCompare = e.getDay;
-	
-	for(int i = 0; i < week.size(); i++)
-	{
-		if(week[i].getDay() == dayToCompare)
-		{
-			w[i].addEvent(e);
-			break;
-		}
 
-	}
+    string dayToCompare = e.getDay;
+
+    for(int i = 0; i < week.size(); i++)
+    {
+        if(week[i].getDay() == dayToCompare)
+        {
+            w[i].addEvent(e);
+            break;
+        }
+
+    }
 }
 
 
@@ -113,28 +105,30 @@ event findLargest(){
 
 }
 
-						   
+
 vector Chunk() {
-        /*
-         * loop through all assignments
-         * go through their individual modularity values
-         * split them into a number of chunks equal to modularity
-         * figure out a way to create a variable amount of variables (DONE)
-         * Ways to create a variable amount of variables:
-         * vector<int> ints;
-         * or
-         * int *px = new int[n]; //(n variables, n is known at runtime)
-         */
+    /*
+     * loop through all assignments
+     * go through their individual modularity values
+     * split them into a number of chunks equal to modularity
+     * figure out a way to create a variable amount of variables (DONE)
+     * Ways to create a variable amount of variables:
+     * vector<int> ints;
+     * or
+     * int *px = new int[n]; //(n variables, n is known at runtime)
+     */
 
-        for(int i = 0; i < events.size() ;i++){
-               int modularity = events[i].getModularity();
-               int time = events[i].getLength();
-               int split = time / modularity;
+    for(int i = 0; i < events.size() ;i++){
+        int modularity = events[i].getModularity();
+        int time = events[i].getLength();
+        int split = time / modularity;
 
-               for(int k =0; k < modularity; k++){
-		       event temp= new event(events[i].getName(), events[i].getDay(), events[i].getTime(), events[i].getRest(), split, events[i].getRepeat());
-		       chunks.push_back(temp);
-               }
+        for(int k =0; k < modularity; k++){
+            event temp= new event(events[i].getName(), events[i].getDay(), events[i].getTime(), events[i].getRest(), split, events[i].getRepeat());
+            chunks.push_back(temp);
+        }
 
-            }
     }
+}
+
+}
